9556d9cd0a62f7af58f0a180fbf0dc34425bad9b
sapphire-al2o3 <t_telkun@hotmail.com>
2023-01-13 16:17:54 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	IEnumerator TestCor()
+	{
+		yield return null;
+		Debug.Log($"Test:0 {Time.frameCount}");
+		yield return null;
+		Debug.Log($"Test:1 {Time.frameCount}");
+		yield return null;
+		Debug.Log($"Test:2 {Time.frameCount}");
+	}
+
+	IEnumerator CallTestA()
+	{
+		Debug.Log($"CallTestA:0 {Time.frameCount}");
+		yield return TestCor();
+		Debug.Log($"CallTestA:1 {Time.frameCount}");
+	}
+
+	IEnumerator CallTestB()
+	{
+		Debug.Log($"CallTestB:0 {Time.frameCount}");
+		yield return StartCoroutine(TestCor());
+		Debug.Log($"CallTestB:1 {Time.frameCount}");
+	}
+
+	IEnumerator Wait()
+	{
+		var e = CallTestB();
+		while (e.MoveNext())
+		{
+			//yield return e.Current;
+		}
+	}
+
+	void Start()
+    {
+		StartCoroutine(Wait());
+    }
+}
