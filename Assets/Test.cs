7c0df8b22fb4baaad38aa826e19a4ba1ed4033ab
sapphire-al2o3 <t_telkun@hotmail.com>
2023-01-22 20:59:28 +0900

    インデント修正

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-	IEnumerator TestCor()
-	{
-		yield return null;
-		Debug.Log($"Test:0 {Time.frameCount}");
-		yield return null;
-		Debug.Log($"Test:1 {Time.frameCount}");
-		yield return new WaitForSeconds(0.1f);
-		Debug.Log($"Test:2 {Time.frameCount}");
-	}
-
-	IEnumerator CallTestA()
-	{
-		Debug.Log($"CallTestA:0 {Time.frameCount}");
-		yield return TestCor();
-		Debug.Log($"CallTestA:1 {Time.frameCount}");
-	}
-
-	IEnumerator CallTestB()
-	{
-		Debug.Log($"CallTestB:0 {Time.frameCount}");
-		yield return StartCoroutine(TestCor());
-		Debug.Log($"CallTestB:1 {Time.frameCount}");
-	}
-
-	IEnumerator Wait()
-	{
-		var e = CallTestB();
-		while (e.MoveNext())
-		{
-			//yield return e.Current;
-			yield return null;
-		}
-	}
-
-	void Start()
+    IEnumerator TestCor()
     {
-		//StartCoroutine(Wait());
-		StartCoroutine(CallTestA());
+        yield return null;
+        Debug.Log($"Test:0 {Time.frameCount}");
+        yield return null;
+        Debug.Log($"Test:1 {Time.frameCount}");
+        yield return new WaitForSeconds(0.1f);
+        Debug.Log($"Test:2 {Time.frameCount}");
+    }
+
+    IEnumerator CallTestA()
+    {
+        Debug.Log($"CallTestA:0 {Time.frameCount}");
+        yield return TestCor();
+        Debug.Log($"CallTestA:1 {Time.frameCount}");
+    }
+
+    IEnumerator CallTestB()
+    {
+        Debug.Log($"CallTestB:0 {Time.frameCount}");
+        yield return StartCoroutine(TestCor());
+        Debug.Log($"CallTestB:1 {Time.frameCount}");
+    }
+
+    IEnumerator Wait()
+    {
+        var e = CallTestB();
+        while (e.MoveNext())
+        {
+            //yield return e.Current;
+            yield return null;
+        }
+    }
+
+    void Start()
+    {
+        //StartCoroutine(Wait());
+        StartCoroutine(CallTestA());
     }
 }
